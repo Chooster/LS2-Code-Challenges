2c647895e179d53f74c34e8df1e3f7b3ec5d4bd2
Chooster <jcooleycoder@gmail.com>
2017-07-27 10:07:37 -0700

    Queue Stack Completed

--- queueStack/queueStack.js
+++ queueStack/queueStack.js
@@ -2,3 +2,45 @@
  * Write a stack class. Once you're done,
  * implement a queue using two stacks.
  */
+
+class Stack {
+  constructor() {
+    this.array = [];
+  }
+  add(value) {
+    return this.array.push(value);
+  }
+  remove() {
+    return this.array.pop();
+  }
+  get size() {
+    return this.array.length;
+  }
+  peek() {
+    return this.array;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.inbox = new Stack();
+    this.outbox = new Stack();
+  }
+  enqueue(element) {
+    this.inbox.add(element);
+  }
+  dequeue() {
+    if (this.outbox.size === 0) {
+      while(this.inbox.size) {
+        this.outbox.add(this.inbox.remove());
+      }
+    }
+    return this.outbox.remove();
+  }
+  get size() {
+    return (this.inbox.size + this.outbox.size);
+  }
+  peek() {
+    return this.outbox.peek();
+  }
+}
