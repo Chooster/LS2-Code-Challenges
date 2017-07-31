acc43668f3f2275a103efeb15324aa2d85c90510
Chooster <jcooleycoder@gmail.com>
2017-07-31 10:31:13 -0700

    Completed All Anagrams

--- allAnagrams/allAnagrams.js
+++ allAnagrams/allAnagrams.js
@@ -11,3 +11,23 @@
   * const anagrams = allAnagrams('abc');
   * console.log(anagrams); // [ 'abc', 'acb', 'bac', 'bca', 'cab', 'cba' ]
   */
+
+const allAnagrams = (str) => {
+  const arr = [str];
+  let tempStr = str.split('');
+  if (str.length < 2) return [str];
+  while(arr.length < 6) {
+    for (let i = 0; i < str.length; i++) {
+      for (let j = 1; j < str.length; j++) {
+        let temp = tempStr[i];
+        tempStr[i] = tempStr[j];
+        tempStr[j] = temp;
+      }
+      if (arr.indexOf(tempStr.join('')) === -1) {
+        arr.push(tempStr.join(''));
+      }
+    }
+    if (arr.indexOf(tempStr.join('')) === -1) arr.push(tempStr.join(''));
+  }
+  return arr;
+};
